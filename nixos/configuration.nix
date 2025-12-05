61d04bec9de27257052930b901fca4199b57fb91
dsoto1219 <dsotomail19@gmail.com>
2025-12-05 21:51:20 +0000

    Use pipewire instead of pulse for audio (latter wasn't working)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,12 +88,10 @@
   services.printing.enable = true;
 
   # Enable sound.
-  services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
