a9eb92a0d8e8a72ec558530d6fdde004bc4b2924
dsoto1219 <dsotomail19@gmail.com>
2025-12-06 15:09:39 -0500

    Remove package attribute from nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -51,7 +51,6 @@
       # flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
-      package = pkgs.nix; # Try to get nix version to 2.4+
     };
     # Opinionated: disable channels
     # channel.enable = false;
