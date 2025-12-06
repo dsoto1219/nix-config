ae45d069260d1a326056e7b9454b7f86ac76473b
dsoto1219 <dsotomail19@gmail.com>
2025-12-06 14:57:54 -0500

    change opinions: re-enable flake-registry and channels

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -48,12 +48,12 @@
       # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
       # Opinionated: disable global registry
-      flake-registry = "";
+      # flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
     };
     # Opinionated: disable channels
-    channel.enable = false;
+    # channel.enable = false;
 
     # Opinionated: make flake registry and nix path match flake inputs
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
