6247d00c455b13ae132e69928502fa14cce33536
dsoto1219 <dsotomail19@gmail.com>
2025-12-06 21:18:29 -0500

    Add bluetooth

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -144,6 +144,9 @@
     # withUWSM = true; 
   };
 
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
   # Optional: hint electron apps to use Wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   
