9ca87e986c9c5edffbc94cb1ccfe273f04865b1b
dsoto1219 <dsotomail19@gmail.com>
2025-12-07 11:55:17 -0500

    fix extraConfig path

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -70,7 +70,7 @@
         natural_scroll = true;
       };
     };
-    extraConfig = builtins.readFile "/home/daniels/.config/hyprland.conf";
+    extraConfig = builtins.readFile "/home/daniels/.config/hypr/hyprland.conf";
   };
 
   home.packages = with pkgs; [ 
