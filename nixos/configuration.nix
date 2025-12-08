db02b4d45fea70cf409f27e0c6e9608edc7099da
dsoto1219 <dsotomail1219@gmail.com>
2025-12-08 18:35:37 +0000

    Change bootloader

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -67,9 +67,13 @@
 
   # FIXME: Add the rest of your current configuration
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
