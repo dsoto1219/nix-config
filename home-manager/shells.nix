2e6abd0f359b3ccf0cbf7e4a3e1d4ae8f978c62c
dsoto1219 <dsotomail1219@gmail.com>
2025-12-09 18:36:46 -0500

    add home.shellAliases

--- home-manager/shells.nix
+++ home-manager/shells.nix
@@ -7,4 +7,11 @@
 }: {
   programs.zsh.enable = true;
   programs.starship.enable = true;
+
+  home.shellAliases = {
+    ls = "ls --group-directories-first --color=auto";
+    # -p adds '/' to the end of directories
+    la = "ls -ap --group-directories-first --color=auto";
+    ll = "ls -lap --group-directories-first --color=auto";
+  };
 }
