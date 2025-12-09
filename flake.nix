eae3d08016f45a47d787f4b53e421958afed4c3f
dsoto1219 <dsotomail1219@gmail.com>
2025-12-09 16:17:03 -0500

    fix: change danim@nixos -> nixos@nixos in home-manager flake output

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
         # > Our main home-manager configuration file <
         modules = [./home-manager/home.nix];
       };
-      "danim@nixos" = home-manager.lib.homeManagerConfiguration {
+      "nixos@nixos" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = {inherit inputs;};
         modules = [./home-manager/home.nix];
