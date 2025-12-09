f479e49a87d3a77e0cd550239f3e6b0d2db01e9e
dsoto1219 <dsotomail1219@gmail.com>
2025-12-09 18:08:12 -0500

    Make zsh the default shell

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -6,10 +6,13 @@
   pkgs,
   ...
 }: {
+  programs.zsh.enable = true; # want zsh to be the default shell
+
   users.users = {
     danim = {
       isNormalUser = true;
       extraGroups = [ "networkmanager" "wheel" ]; # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      shell = pkgs.zsh;
     };
   };
 }
