cf13d74ecd1d5e5b95d05bfeba0f0d1b15e5e48c
dsoto1219 <dsotomail1219@gmail.com>
2025-12-09 15:25:52 -0500

    Moved shared options between machines to common/default.nix

---
+++ nixos/common/default.nix
@@ -0,0 +1,121 @@
+# Configuration options shared across machines.
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      # flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    # channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+    # Garbage collection
+    gc = {
+      automatic = true;
+      dates = "daily";
+    };
+  };
+
+  # Automatic upgrading
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  users.users = {
+    danim = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" ]; # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+    };
+  };
+
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Opinionated: forbid root login through SSH.
+      # PermitRootLogin = "no";
+      # Opinionated: use keys only.
+      # Remove if you want to SSH using passwords
+      # PasswordAuthentication = false;
+    };
+  };
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    lf
+    wget
+    tmux
+    git
+    upower
+  ];
+  environment.variables = {
+    EDITOR = "vim";
+  };
+
+  # Use Hyprland
+  programs.hyprland = let
+    system = pkgs.stdenv.hostPlatform.system;
+  in {
+    enable = true;
+    # For flake:
+    package = inputs.hyprland.packages."${system}".hyprland; 
+    # Use Universal Wayland Session Manager---recommended way of launching Hyprland, as it neatly integrates with systemd.
+    # withUWSM = true; 
+  };
+
+  # Optional: hint electron apps to use Wayland:
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  
+  programs.firefox.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "25.11";
+}
