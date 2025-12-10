5a6d7a50b43b90b086ee762976ae896892af48ee
dsoto1219 <dsotomail19@gmail.com>
2025-12-09 21:32:58 -0500

    Add alias to get device battery life from upower

--- home-manager/shells.nix
+++ home-manager/shells.nix
@@ -12,5 +12,8 @@
     ls = "ls --group-directories-first --color=auto";
     la = "ls -a";
     ll = "ls -la";
+
+    # Alias to get battery life info from command line (assuming upower is enabled)
+    bat = "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E 'state|time to full|percentage'";
   };
 }
