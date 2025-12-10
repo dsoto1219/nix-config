ebf06edcc591e69c86f953c6322c00926ca935c4
dsoto1219 <dsotomail19@gmail.com>
2025-12-09 23:28:51 -0500

    try disabling greetd

--- nixos/common/hyprland.nix
+++ nixos/common/hyprland.nix
@@ -30,7 +30,7 @@
 
   # Login Manager: ReGreet
   programs.regreet.enable = true;
-  services.greetd.enable = true;
+  # services.greetd.enable = true;
 
   # Optional: hint electron apps to use Wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
