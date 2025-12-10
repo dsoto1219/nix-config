d4c033d2bb7ba6fd9d36ea35ea2a2d5a9ef627a1
dsoto1219 <dsotomail19@gmail.com>
2025-12-09 19:57:13 -0500

    Add X11 windowing system and keybinds

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -104,6 +104,13 @@
     nerd-fonts.hack
   ];
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
+
   # Use Hyprland
   programs.hyprland = let
     system = pkgs.stdenv.hostPlatform.system;
