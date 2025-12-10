5ee03ff8a74518da1a582113d70a69fade239d13
dsoto1219 <dsotomail19@gmail.com>
2025-12-09 23:16:43 -0500

    Revert "Add hyprland home-manager configuration to home-manager flake modules"
    This reverts commit a57535c956dab8ff5e0f2f7d3662be6624227c78.

--- flake.nix
+++ flake.nix
@@ -60,7 +60,6 @@
         # > Our main home-manager configuration file <
         modules = [
 	  ./home-manager/home.nix
-	  ./home-manager/hypr/hypr.nix
 	  ./home-manager/users/danim.nix
 	];
       };
@@ -69,7 +68,6 @@
         extraSpecialArgs = { inherit inputs; };
         modules = [
 	  ./home-manager/home.nix
-	  ./home-manager/hypr/hypr.nix
 	  ./home-manager/users/nixos.nix
 	];
       };
