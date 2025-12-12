171d7026b5c6a2eaf3db7aecc07df628a70dfa15
dsoto1219 <dsotomail19@gmail.com>
2025-12-12 12:12:15 -0500

    fix: extraConfig listed as a setting attribute instead of hyprland attribute

--- home-manager/hypr/hypr.nix
+++ home-manager/hypr/hypr.nix
@@ -9,14 +9,16 @@ in {
   # Hyprland Configuration
   programs.kitty.enable = true;
   wayland.windowManager.hyprland.systemd.variables = ["--all"];
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER"; 
-    bind = [
-      "$mod, F, fullscreen"
-    ];
-    input = {
-      touchpad = {
-        natural_scroll = true;
+  wayland.windowManager.hyprland = {
+    settings = {
+      "$mod" = "SUPER"; 
+      bind = [
+        "$mod, F, fullscreen"
+      ];
+      input = {
+        touchpad = {
+          natural_scroll = true;
+        };
       };
     };
     extraConfig = builtins.readFile ./hyprland.conf;
