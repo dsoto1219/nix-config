eae0bdb63957ec180d79073cb68f744bec35370b
dsoto1219 <dsotomail19@gmail.com>
2025-12-15 14:48:11 -0500

    mini-statusline: enable using icons (I know nerd fonts are instaleld)

--- home-manager/nvim/init.nix
+++ home-manager/nvim/init.nix
@@ -17,7 +17,12 @@
 
     imports = [ ./options.nix ./keymaps.nix ];
 
-    plugins.mini-statusline.enable = true;
+    plugins.mini-statusline = {
+      enable = true;
+      settings = {
+        use_icons = true;
+      };
+    };
     plugins.guess-indent.enable = true;
 
     colorschemes.moonfly.enable = true;
