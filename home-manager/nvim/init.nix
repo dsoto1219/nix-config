ddf99e6cf0964087fc9b3dfbc4b9ce21aaba6a79
dsoto1219 <dsotomail19@gmail.com>
2025-12-15 14:44:47 -0500

    Replace lualine with mini-statusline

--- home-manager/nvim/init.nix
+++ home-manager/nvim/init.nix
@@ -17,7 +17,7 @@
 
     imports = [ ./options.nix ./keymaps.nix ];
 
-    plugins.lualine.enable = true;
+    plugins.mini-statusline.enable = true;
     plugins.guess-indent.enable = true;
 
     colorschemes.moonfly.enable = true;
