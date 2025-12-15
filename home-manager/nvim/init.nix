97c4e09b8396dc853b8b0a1f4311f51204913ab2
dsoto1219 <dsotomail19@gmail.com>
2025-12-15 14:12:00 -0500

    Add more neovim options

--- home-manager/nvim/init.nix
+++ home-manager/nvim/init.nix
@@ -25,6 +25,40 @@
       relativenumber = true;
 
       showmode = false; # disabled, lualine handles this
+
+      breakindent = true;
+
+      # Save undo history
+      undofile = true;
+
+      # Keep sign column on by default
+      signcolumn = "yes";
+
+      # Decrease update time
+      updatetime = 250;
+      # Decrease mapped sequence wait time
+      timeoutlen = 300;
+
+      # Configure how new splits should be opened
+      # splitright = true;
+      # splitbelow = true;
+
+      # Sets how neovim will display certain whitspace characters in the editor
+      list = true;
+      listchars = { tab = '» ', trail = '·', nbsp = '␣' };
+
+      # Preview substitutions live, as you type!
+      inccommand = "split";
+
+      # Show which line your cursor is on
+      cursorline = true;
+
+      # Minimal number of screen lines to keep above and below the cursor
+      scrolloff = 10;
+
+      # if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+      # instead raise a dialog asking if you wish to save the current file(s)
+      confirm = true;
     };
 
     plugins.lualine.enable = true;
