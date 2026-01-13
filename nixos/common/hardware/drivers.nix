241473b79cecbaa12d2376f727f66d2d17eeb984
dsoto1219 <dsotomail19@gmail.com>
2026-01-13 15:27:56 -0500

    Enable wpa_gui and wpa_cli

--- nixos/common/hardware/drivers.nix
+++ nixos/common/hardware/drivers.nix
@@ -14,6 +14,8 @@
     networkmanagerapplet # enable nmapplet & nm-connection-editor
   ];
   networking.networkmanager.wifi.powersave = true;
+  # Enable wpa_gui and wpa_cli
+  networking.wireless.enable = true;
 
   # Enable bluetooth
   hardware.bluetooth.enable = true;
