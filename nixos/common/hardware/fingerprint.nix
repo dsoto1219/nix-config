6f107fd84ed48da05f0f64d9321242fc7659d32f
dsoto1219 <dsotomail19@gmail.com>
2026-01-14 17:28:26 -0500

    fingerprint: vfs0090 driver didn't work, try goodix driver

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -7,6 +7,7 @@
   services.fprintd.enable = true;
   services.fprintd.tod.enable = true;
 
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; # If the vfs0090 Driver does not work, use the following driver)
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # On my device it only worked with this driver)
+  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; 
+  # If the vfs0090 Driver does not work, use the following driver
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 }
