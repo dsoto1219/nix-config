a3a8ee17de84e280d30cdb035afa3e733cb52117
dsoto1219 <dsotomail19@gmail.com>
2026-01-14 15:20:00 -0500

    Add drawing tablet drivers

---
+++ nixos/common/hardware/tablets.nix
@@ -0,0 +1,10 @@
+# Configuration for drawing tablets
+{ ... }:
+{
+  # Enable OpenTabletDriver
+  hardware.opentabletdriver.enable = true;
+
+  # Required by OpenTabletDriver
+  hardware.uinput.enable = true;
+  boot.kernelModules = [ "uinput" ];
+}
