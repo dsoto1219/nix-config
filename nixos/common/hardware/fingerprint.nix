6f654c4001859fe720647f4163bb069493d06e56
dsoto1219 <dsotomail19@gmail.com>
2026-01-15 13:27:28 -0500

    Revert "Add back fingerprint drivers"
    This reverts commit ba7ae4b8343fd0161ecd72d44595585f58168265.

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
   services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+  # services.fprintd.tod.enable = true;
+
+  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; 
+  # If the vfs0090 Driver does not work, use the following driver
+  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 }
