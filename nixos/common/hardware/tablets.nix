4911b35ac5de4d8b0e88952b08187c57304775b5
dsoto1219 <dsotomail19@gmail.com>
2026-01-15 15:20:11 -0500

    Add keybinds for drawing tablet

--- nixos/common/hardware/tablets.nix
+++ nixos/common/hardware/tablets.nix
@@ -7,4 +7,15 @@
   # Required by OpenTabletDriver
   hardware.uinput.enable = true;
   boot.kernelModules = [ "uinput" ];
+
+  # Add keybinds
+  services.udev.enable = true;
+  services.udev.extraHwdb =''
+    evdev:input:b<BUS-ID>v<VID>p<PID>*
+      KEYBOARD_KEY_<HOTKEY-ID>=<KEY-SCAN-CODE>
+      KEYBOARD_KEY_70005=h
+      KEYBOARD_KEY_700e0=0x1d
+      KEYBOARD_KEY_70057=a
+      KEYBOARD_KEY_70056=z
+  '';
 }
