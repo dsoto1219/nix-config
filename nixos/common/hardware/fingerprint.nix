4d558cb097384301151aaee1a6d3d84535e40c73
dsoto1219 <dsotomail19@gmail.com>
2026-01-15 13:28:07 -0500

    Enable fprintAuth for login

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -6,4 +6,5 @@
   # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; 
   # If the vfs0090 Driver does not work, use the following driver
   # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+  security.pam.services.login.fprintAuth = true;
 }
