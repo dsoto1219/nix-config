8056ffa918d48fc26ea9cc0562477b0dcaaacc8c
dsoto1219 <dsotomail19@gmail.com>
2026-01-16 10:39:15 -0500

    Add tablet ids (all zeros?)

--- nixos/common/hardware/tablets.nix
+++ nixos/common/hardware/tablets.nix
@@ -10,9 +10,9 @@
 
   # Add keybinds
   services.udev.enable = true;
+  # 056a:0374 Input device ID: bus 0000 vendor 0000 product 0000 version 0000
   services.udev.extraHwdb =''
-    evdev:input:b<BUS-ID>v<VID>p<PID>*
-      KEYBOARD_KEY_<HOTKEY-ID>=<KEY-SCAN-CODE>
+    evdev:input:b0000v0000p0000*
       KEYBOARD_KEY_70005=h
       KEYBOARD_KEY_700e0=0x1d
       KEYBOARD_KEY_70057=a
