f859867f83c343e01b6d0499b1459db3ff71bf8c
dsoto1219 <dsotomail19@gmail.com>
2026-01-16 12:07:55 -0500

    Add open-fprintd to system packages instead of specifying services.fprint package

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -1,13 +1,9 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    open-fprintd
     libfprint libfprint-tod
   ];
-
-  services.fprintd = {
-    enable = true;
-    package = pkgs.open-fprintd;
-  };
 
   security.pam.services.login.fprintAuth = true;
 }
