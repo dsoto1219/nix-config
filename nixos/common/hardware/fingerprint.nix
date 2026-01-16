27f5280b08df96438d419ce1d626573ffe40d529
dsoto1219 <dsotomail19@gmail.com>
2026-01-16 12:22:58 -0500

    Add open-fprintd configuration to fingerprint.nix

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -5,5 +5,10 @@
     libfprint libfprint-tod
   ];
 
+  services."06cb-009a-fingerprint-sensor" = {
+    enable = true;
+    backend = "python-validity";
+  };
+
   security.pam.services.login.fprintAuth = true;
 }
