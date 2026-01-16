78b38fc77f79372b3aec1290591f757326b1928d
dsoto1219 <dsotomail19@gmail.com>
2026-01-16 11:43:27 -0500

    Add fingerprint drivers for goodix 550a

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   services.fprintd.enable = true;
-  # services.fprintd.tod.enable = true;
+  services.fprintd.tod.enable = true;
 
   # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; 
   # If the vfs0090 Driver does not work, use the following driver
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
   security.pam.services.login.fprintAuth = true;
 }
