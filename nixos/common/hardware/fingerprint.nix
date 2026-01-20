66485500d5ede43d3cfeccc377f49122aa97f068
dsoto1219 <dsotomail19@gmail.com>
2026-01-20 14:15:48 -0500

    Revert "Add open-fprintd configuration to fingerprint.nix"
    This reverts commit 27f5280b08df96438d419ce1d626573ffe40d529.

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -5,10 +5,5 @@
     libfprint libfprint-tod
   ];
 
-  services."06cb-009a-fingerprint-sensor" = {
-    enable = true;
-    backend = "python-validity";
-  };
-
   security.pam.services.login.fprintAuth = true;
 }
