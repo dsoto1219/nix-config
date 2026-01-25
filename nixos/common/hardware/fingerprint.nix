11c7640dc456adad65b75577f61ec51960a73bb1
dsoto1219 <dsotomail19@gmail.com>
2026-01-25 14:24:10 -0500

    Undo stupid idea, add back normal 550a driver

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -3,7 +3,7 @@
   services.fprintd = {
     enable = true;
     tod.enable = true;
-    tod.driver = pkgs.libfprint-2-tod1-goodix-55a4;
+    tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
   };
 
   security.pam.services.login.fprintAuth = true;
