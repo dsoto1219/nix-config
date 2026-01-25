ff95a0fdfacf6a752afedd5aab3d5369d18aa8f7
dsoto1219 <dsotomail19@gmail.com>
2026-01-25 14:22:21 -0500

    Try stupid idea

--- nixos/common/hardware/fingerprint.nix
+++ nixos/common/hardware/fingerprint.nix
@@ -3,7 +3,7 @@
   services.fprintd = {
     enable = true;
     tod.enable = true;
-    tod.driver = pkgs.libfprint-2-tod1-goodix;
+    tod.driver = pkgs.libfprint-2-tod1-goodix-55a4;
   };
 
   security.pam.services.login.fprintAuth = true;
