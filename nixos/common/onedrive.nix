289816b64ea756208ee52d1d2e8c3e1df4081366
dsoto1219 <dsotomail19@gmail.com>
2026-01-28 13:33:57 -0500

    Add onedriver package

--- nixos/common/onedrive.nix
+++ nixos/common/onedrive.nix
@@ -1,5 +1,10 @@
 # Single line enabling onedrive, adding like this for modularity's sake
-{ ... }:
+{ pkgs, ... }:
 {
   services.onedrive.enable = true;
+
+  # Enable onedriver for filesystem
+  environment.systemPackages = with pkgs; [
+    onedriver
+  ];
 }
