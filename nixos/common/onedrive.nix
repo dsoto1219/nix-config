18e03e5ff983cdc4b1d6477abddcbcc820ecf252
dsoto1219 <dsotomail19@gmail.com>
2026-01-28 13:37:01 -0500

    Remove services.onedrive.enable from onedrive.nix

--- nixos/common/onedrive.nix
+++ nixos/common/onedrive.nix
@@ -1,8 +1,6 @@
 # Single line enabling onedrive, adding like this for modularity's sake
 { pkgs, ... }:
 {
-  services.onedrive.enable = true;
-
   # Enable onedriver for filesystem
   environment.systemPackages = with pkgs; [
     onedriver
