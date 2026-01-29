36cf1c0ebec3da2fb25d95764279533f684cf277
dsoto1219 <dsotomail19@gmail.com>
2026-01-29 18:52:16 -0500

    Replace %h with ~ in rclone systemd process

--- home-manager/rclone.nix
+++ home-manager/rclone.nix
@@ -12,8 +12,8 @@
     };
     Service = {
       Type = "notify";
-      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/${directory}/";
-      ExecStart = "${pkgs.rclone}/bin/rclone --config=%h/.config/rclone/rclone.conf --vfs-cache-mode writes --ignore-checksum mount \"${remote-name}:\" \"%h/${directory}\"";
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ~/${directory}/";
+      ExecStart = "${pkgs.rclone}/bin/rclone --vfs-cache-mode writes --ignore-checksum mount ${remote-name}: ~/${directory}";
       ExecStop="/run/wrappers/bin/fusermount -u %h/${directory}/%i";
     };
     Install.WantedBy = [ "default.target" ];
