c11a445900372aab0238778f9c1179c6aaa7ce0b
dsoto1219 <dsotomail19@gmail.com>
2026-01-29 13:28:22 -0500

    Add rclone

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -50,6 +50,7 @@
     webkitgtk_6_0
     gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad
     ## End onedriver packages
+    rclone
     obsidian 
     zotero
     zathura
