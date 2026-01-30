f0684c48a78bbacfb4178a03878defae894eaef5
dsoto1219 <dsotomail19@gmail.com>
2026-01-30 13:08:48 -0500

    Add flags to speed up VFS

--- home-manager/rclone.nix
+++ home-manager/rclone.nix
@@ -13,7 +13,11 @@
     Service = {
       Type = "simple";
       ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/${directory}/";
-      ExecStart = "${pkgs.rclone}/bin/rclone --vfs-cache-mode writes -vv --ignore-checksum mount ${remote-name}: %h/${directory}/";
+      ExecStart = ''${pkgs.rclone}/bin/rclone 
+        --vfs-cache-mode writes -vv 
+        --no-checksum --no-modtime --no-seek 
+        mount ${remote-name}: %h/${directory}/
+      '';
       ExecStop="/run/wrappers/bin/fusermount -u -vv %h/${directory}/%i";
     };
     Install.WantedBy = [ "default.target" ];
