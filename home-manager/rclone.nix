e2dd4c912432dddf3035eddb52e3cc4766e977db
dsoto1219 <dsotomail19@gmail.com>
2026-01-30 11:04:02 -0500

    Add quotes around ExecStart directory for rclone mount service

--- home-manager/rclone.nix
+++ home-manager/rclone.nix
@@ -13,7 +13,7 @@
     Service = {
       Type = "notify";
       ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/${directory}/";
-      ExecStart = "${pkgs.rclone}/bin/rclone --vfs-cache-mode writes --verbose --ignore-checksum mount ${remote-name}: %h/${directory}";
+      ExecStart = "${pkgs.rclone}/bin/rclone --vfs-cache-mode writes --verbose --ignore-checksum mount ${remote-name}: \"%h/${directory}\"";
       ExecStop="/run/wrappers/bin/fusermount -u --verbose %h/${directory}/%i";
     };
     Install.WantedBy = [ "default.target" ];
