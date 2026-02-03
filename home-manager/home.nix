8c7f7efdb936b7d87a9ac86c5438e9dfa60fc5d1
dsoto1219 <dsotomail19@gmail.com>
2026-02-03 13:32:33 -0500

    Remove onedriver.nix import (until onedriver is updated)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,7 +14,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     ./shells.nix
-    ./onedriver.nix
+    # ./onedriver.nix
     ./hypr/hyprland.nix
     ./nvim/init.nix
   ];
