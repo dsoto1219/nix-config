ae1c620d3c4e09ea48fa8116ad94ad3f4c7cdd67
dsoto1219 <dsotomail19@gmail.com>
2026-02-06 12:08:20 -0500

    Add comment letting people know that my hyprland config is against docs

--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -1,7 +1,7 @@
 { inputs, lib, config, pkgs, ... }:
 {
   imports = [
-    inputs.hyprland.homeManagerModules.default
+    inputs.hyprland.homeManagerModules.default # this shouldn't work---it's not how the docs say to do it---but it does
     ./waybar.nix
   ];
 
