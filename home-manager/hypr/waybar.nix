6f4c3772e599f1567aa8ece1c87700b08bde1358
dsoto1219 <dsotomail19@gmail.com>
2026-02-06 12:36:33 -0500

    fix: forgot semicolon

--- home-manager/hypr/waybar.nix
+++ home-manager/hypr/waybar.nix
@@ -69,7 +69,7 @@
     network = {
       format = "󰖩  {essid}";
       format-disconnected = "󰖪  disconnected";
-      exec = "nm-applet"
+      exec = "nm-applet";
     };
     clock = {
       format = "  {:%I:%M %p    %m/%d} ";
