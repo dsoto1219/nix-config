6f5f053a85af3471f9fb0a0fa17a025c739e655a
dsoto1219 <dsotomail19@gmail.com>
2026-02-08 16:12:36 -0500

    Add monitor argument to wallpaper

--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -45,7 +45,7 @@
   services.hyprpaper.enable = true; # dynamic wallpaper manager
   services.hyprpaper.settings = {
     wallpaper = [
-      ",path = ../../../assets/planet-bottom.jpg"
+      "DP-1,path = ../../../assets/planet-bottom.jpg"
     ];
   };
 }
