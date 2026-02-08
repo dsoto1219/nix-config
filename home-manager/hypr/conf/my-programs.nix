d9fe70afd1f2ff37f81d88cc103da64c06a428ea
dsoto1219 <dsotomail19@gmail.com>
2026-02-08 13:27:58 -0500

    Track my-programs.nix

---
+++ home-manager/hypr/conf/my-programs.nix
@@ -0,0 +1,15 @@
+###################
+### MY PROGRAMS ###
+###################
+
+# See https://wiki.hypr.land/Configuring/Keywords/
+{ ... }:
+{
+
+wayland.windowManager.hyprland.settings = {
+    # Set programs that you use
+    "$terminal" = "kitty";
+    "$fileManager" = "dolphin";
+    "$menu" = "wofi --show drun";
+  };
+}
