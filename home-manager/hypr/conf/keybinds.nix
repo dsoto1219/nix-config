6aa7dee510ecc49af6946da32661fb1259730a17
dsoto1219 <dsotomail19@gmail.com>
2026-02-08 15:06:08 -0500

    bind SUPER+M to wlogout

--- home-manager/hypr/conf/keybinds.nix
+++ home-manager/hypr/conf/keybinds.nix
@@ -14,7 +14,7 @@
     bind = [
       "$mainMod, Q, exec, $terminal"
       "$mainMod, C, killactive"
-      "$mainMod, M, exit"
+      "$mainMod, M, wlogout"
       "$mainMod, E, exec, $fileManager"
       "$mainMod, V, togglefloating"
       "$mainMod, R, exec, $menu"
