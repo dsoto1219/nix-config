b792d3ee795ebfbf25fad75c9b8cd383a876d6b5
dsoto1219 <dsotomail19@gmail.com>
2026-02-08 15:57:14 -0500

    Re-enable onedriver in home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,7 +14,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     ./shells.nix
-    # ./onedriver.nix
+    ./onedriver.nix
     ./nix-colors.nix
     ./hypr/hyprland.nix
     ./nvim/init.nix
