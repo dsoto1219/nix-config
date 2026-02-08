6169ac15b2a31135eaf5e06aa9905fe98737d7e5
dsoto1219 <dsotomail19@gmail.com>
2026-02-08 14:33:27 -0500

    Move things unrelated to base hyprland config to extra/ directory

--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -2,14 +2,10 @@
 {
   imports = [
     inputs.hyprland.homeManagerModules.default # this shouldn't work---it's not how the docs say to do it---but it does
-    ./waybar.nix
 
     # Split config into multiple files
     ./conf
-  ];
-
-  home.packages = with pkgs; [
-    unstable.hyprshutdown
+    ./extra
   ];
 
   # Hyprland Configuration
@@ -22,27 +18,4 @@
     portalPackage = hyprland-pkgs.xdg-desktop-portal-hyprland; 
     systemd.variables = ["--all"];
   };
-
-  services.dunst.enable = true; # notification manager
-  programs.hyprshot.enable = true; # screenshot manager
-
-  programs.hyprlock.enable = true;
-  xdg.configFile.hyprlock = {
-    source = ./hyprlock.conf;
-    target = "hypr/hyprlock.conf";
-  };
-
-  services.hypridle.enable = true;
-  xdg.configFile.hypridle = {
-    source = ./hypridle.conf;
-    target = "hypr/hypridle.conf";
-  };
-
-  services.hyprpaper.enable = true; # dynamic wallpaper manager
-  services.hyprpaper.settings = {
-    wallpaper = [
-      ",path = ../../assets/planet-bottom.jpg"
-    ];
-  };
-  programs.wlogout.enable = true;
 }
