8384ff56076a64f4c38aa4c62f936b5eae776a77
dsoto1219 <dsotomail19@gmail.com>
2026-02-08 16:24:11 -0500

    wlogout: change hyprshutdown post-cmd from shutdown to shutdown now

--- home-manager/hypr/wlogout.nix
+++ home-manager/hypr/wlogout.nix
@@ -13,7 +13,7 @@
     {
       "label" = "shutdown";
       # https://github.com/hyprwm/Hyprland/blob/main/example/hyprland.conf @ line 240
-      "action" = "hyprshutdown --verbose >/dev/null 2>&1 && hyprshutdown --verbose --post-cmd shutdown || hyprctl dispatch exit";
+      "action" = "hyprshutdown --verbose >/dev/null 2>&1 && hyprshutdown --verbose --post-cmd shutdown now || hyprctl dispatch exit";
       "text" = "Shutdown";
       "keybind" = "s";
     }
