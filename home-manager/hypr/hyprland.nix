1e4f39b8dc3d45114c0a6f0ed64153d64356038f
dsoto1219 <dsotomail19@gmail.com>
2026-02-09 13:01:47 -0500

    hyprpaper: add preload for wallpaper

--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -42,6 +42,9 @@
 
   services.hyprpaper.enable = true; # dynamic wallpaper manager
   services.hyprpaper.settings = {
+    preload = [
+      "~/nix-config/assets/planet-bottom.jpg"
+    ];
     wallpaper = [
       # By default/fallback
       ",~/nix-config/assets/planet-bottom.jpg"
