43c2731f493e0f81f06932f4ef9a346dde62a61b
dsoto1219 <dsotomail19@gmail.com>
2026-02-09 14:57:24 -0500

    Change wallpaper to house-in-middle-of-mountain.jpg

--- home-manager/hypr/hyprpaper.nix
+++ home-manager/hypr/hyprpaper.nix
@@ -3,11 +3,11 @@
   services.hyprpaper.enable = true; # dynamic wallpaper manager
   services.hyprpaper.settings = {
     preload = [
-      "~/nix-config/assets/planet-bottom.jpg"
+      "~/nix-config/assets/house-in-middle-of-mountain.jpg"
     ];
     wallpaper = [
       # By default/fallback
-      ",~/nix-config/assets/planet-bottom.jpg"
+      ",~/nix-config/assets/house-in-middle-of-mountain.jpg"
     ];
   };
 }
