f7db2190c126d492334960ac2a01163cc974c37e
dsoto1219 <dsotomail19@gmail.com>
2026-02-09 12:37:55 -0500

    hyprland: disable forcing default wallpaper

--- home-manager/hypr/conf/look-and-feel.nix
+++ home-manager/hypr/conf/look-and-feel.nix
@@ -115,8 +115,8 @@
 
     # https://wiki.hypr.land/Configuring/Variables/#misc
     misc = {
-      force_default_wallpaper = 2; # Set to 0 or 1 to disable the anime mascot wallpapers
-      disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
+      # force_default_wallpaper = 2; # Set to 0 or 1 to disable the anime mascot wallpapers
+      # disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
     };
   };
 }
