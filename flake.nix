7b5c27075c578c9e3e2944b8f4e150e97ab253fe
dsoto1219 <dsotomail19@gmail.com>
2026-02-09 16:25:02 -0500

    Set stylix version

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     nixvim.url = "github:nix-community/nixvim";
 
     # stylix
-    stylix.url = "github:nix-community/stylix";
+    stylix.url = "github:nix-community/stylix/release-25.11";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
