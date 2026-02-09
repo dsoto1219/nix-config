2cc834a3f0f8bba5ed240351b7810287f58f3e8e
dsoto1219 <dsotomail19@gmail.com>
2026-02-09 18:38:35 -0500

    waybar: fix volume spacing issues

--- home-manager/hypr/waybar.nix
+++ home-manager/hypr/waybar.nix
@@ -76,7 +76,7 @@
         <tt><small>{calendar}</small></tt>'';
     };
     pulseaudio = {
-      format = "{icon}   {volume}%";
+      format = "{icon} {volume}%";
       tooltip = false;
       format-muted = " Muted";
       on-click = "pamixer -t";
