bf32256b24579264012ca48dfc343ea3799bc561
dsoto1219 <dsotomail19@gmail.com>
2026-02-10 12:51:23 -0500

    Revert "Add profile as stylix target for firefox"
    This reverts commit d55bf0c894f3ef34a6e3de67231f1cd2effd9ddd.

--- home/home.nix
+++ home/home.nix
@@ -72,7 +72,6 @@
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   stylix.image = ../assets/house-in-middle-of-mountain.jpg;
-  stylix.targets.firefox.profileNames = [ "danimationso.0@gmail.com" ];
 
   # Let home-manager manage itself
   programs.home-manager.enable = true;
