638b14efb67ae24b45746b866ff96d4506f1e8a5
dsoto1219 <dsotomail19@gmail.com>
2026-02-10 10:47:39 -0500

    Remove lf from system packages

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -93,7 +93,6 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    lf
     wget
     tmux
     git
