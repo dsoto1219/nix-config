7c979736a8ac8b32444394f8ad577aad6803f86b
dsoto1219 <dsotomail19@gmail.com>
2026-02-09 21:31:50 -0500

    Revert "Add ssh-agent as an antidote plugin"
    This reverts commit 173456dec224a7e5fc43e9f225bf01a7d99bf91f.

--- home/shells.nix
+++ home/shells.nix
@@ -11,9 +11,6 @@
    autoload -U compinit && compinit
    zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}';
   '';
-  programs.zsh.antidote.plugins = [
-    "hkupty/ssh-agent"
-  ];
   programs.starship.enable = true;
 
   home.shellAliases = {
