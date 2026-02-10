f2ed2628d9278b0c5a7d7ab61c6a28e9397da127
dsoto1219 <dsotomail19@gmail.com>
2026-02-09 19:53:19 -0500

    overlays/default.nix: replace final.system with final.stdenv.hostPlatform.system

--- overlays/default.nix
+++ overlays/default.nix
@@ -33,7 +33,7 @@
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
