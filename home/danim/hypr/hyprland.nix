10d6466153295e6dbfc6b6e7b1ac45acba878a62
dsoto1219 <dsotomail19@gmail.com>
2026-02-11 12:27:36 -0500

    hyprland: comment out all cursor settings

--- home/danim/hypr/hyprland.nix
+++ home/danim/hypr/hyprland.nix
@@ -294,16 +294,16 @@
   };
 
   # Cursor settings #
-  home.pointerCursor = {
-    gtk.enable = true;
-    # x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 16;
-  };
-
-  gtk = {
-    enable = true;
+  # home.pointerCursor = {
+  #   gtk.enable = true;
+  #   # x11.enable = true;
+  #   package = pkgs.bibata-cursors;
+  #   name = "Bibata-Modern-Classic";
+  #   size = 16;
+  # };
+  #
+  # gtk = {
+  #   enable = true;
   #   theme = {
   #     package = pkgs.flat-remix-gtk;
   #     name = "Flat-Remix-GTK-Grey-Darkest";
