393ad931926dd6e076a2831055271356106bd9df
dsoto1219 <dsotomail19@gmail.com>
2026-02-12 16:57:57 -0500

    firefox.nix: comment out xdg.mimeApps

--- home/danim/firefox.nix
+++ home/danim/firefox.nix
@@ -145,10 +145,10 @@
   #   };
   # };
 
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = ["firefox.desktop"];
-    "text/xml" = ["firefox.desktop"];
-    "x-scheme-handler/http" = ["firefox.desktop"];
-    "x-scheme-handler/https" = ["firefox.desktop"];
-  };
+  # xdg.mimeApps.defaultApplications = {
+  #   "text/html" = ["firefox.desktop"];
+  #   "text/xml" = ["firefox.desktop"];
+  #   "x-scheme-handler/http" = ["firefox.desktop"];
+  #   "x-scheme-handler/https" = ["firefox.desktop"];
+  # };
 }
