87c8d2191cbf21ad851c8851cb2d532e97f3bd44
dsoto1219 <dsotomail19@gmail.com>
2026-02-12 13:26:50 -0500

    Add firefox-addons to flake inputs

--- flake.nix
+++ flake.nix
@@ -33,6 +33,11 @@
     # stylix
     stylix.url = "github:nix-community/stylix/release-25.11";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
