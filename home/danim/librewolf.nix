6a6fc7103beb4caea4da25b6c89cfc1960d5e410
dsoto1219 <dsotomail19@gmail.com>
2026-02-13 13:32:17 -0500

    librewolf: Add search engines to danim profile

--- home/danim/librewolf.nix
+++ home/danim/librewolf.nix
@@ -13,7 +13,27 @@
         pkgs.tridactyl-native
       ];
     };
-    profiles.danim = {};
+    profiles.danim = {
+      search = {
+        force = true;
+        default = "ecosia";
+        privateDefault = "ecosia";
+        order = ["ecosia" "ddg" "google"];
+        engines = {
+          ecosia = {
+            name = "ecosia";
+            urls = [{
+              template = "https://ecosia.org/search?q={searchTerms}";}
+            ];
+            icon = "https://ecosia.org/favicon.ico";
+          };
+          bing.metaData.hidden = true;
+          google.metaData = {
+            hidden = true;
+            alias = "@g";
+          };
+        };
+    };
     # Enable WebGL, cookies and history
     settings = {
       "webgl.disabled" = false;
