556c931b75d2a9cb6ff82af1599a3b539a7902d3
dsoto1219 <dsotomail19@gmail.com>
2026-02-13 13:18:45 -0500

    librewolf: add gnome-shell-connector and tridactyl

--- home/danim/librewolf.nix
+++ home/danim/librewolf.nix
@@ -1,9 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
   stylix.targets.librewolf.profileNames = [ "danim" ];
 
   programs.librewolf = {
     enable = true;
+    packages = pkgs.librewolf.override {
+      # See nixpkgs' firefox/wrapper.nix to check which options you can use
+      nativeMessagingHosts = [
+        # Gnome shell native connector
+        pkgs.gnome-browser-connector
+        # Tridactyl native connector
+        pkgs.tridactyl-native
+      ];
+    };
     profiles.danim = {};
     # Enable WebGL, cookies and history
     settings = {
