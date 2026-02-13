906f990757b9f36cfe4563e21891080daef37e05
dsoto1219 <dsotomail19@gmail.com>
2026-02-13 13:19:48 -0500

    librewolf: fix typo: .packages -> .package

--- home/danim/librewolf.nix
+++ home/danim/librewolf.nix
@@ -4,7 +4,7 @@
 
   programs.librewolf = {
     enable = true;
-    packages = pkgs.librewolf.override {
+    package = pkgs.librewolf.override {
       # See nixpkgs' firefox/wrapper.nix to check which options you can use
       nativeMessagingHosts = [
         # Gnome shell native connector
