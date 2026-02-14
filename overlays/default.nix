7713647703a2207ff262b5c5d4f95344147858c8
dsoto1219 <dsotomail19@gmail.com>
2026-02-13 20:32:48 -0500

    Remove onedriver overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,24 +10,6 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    onedriver = prev.onedriver.overrideAttrs (
-      newAttrs: oldAttrs: {
-        version = "0.15.0";
-        src = final.pkgs.fetchFromGitHub {
-          inherit (oldAttrs.src) owner repo;
-          rev = "v${newAttrs.version}";
-          hash = "sha256-DCxF52CtA9KAP+yz5Rgzc/nUAXtZwfYAVU7oHREJlRY=";
-        };
-        # new dependencies hash got from: nixpkgs-update-log
-        # https://nixpkgs-update-logs.nix-community.org/onedriver/2026-01-31.log
-        vendorHash = "sha256-Ifcmf9AtZnrjgTPQnof/ap0TY19zHVftm5N4JgvbAgs=";
-        # Desktop file name changed in:
-        # https://github.com/jstaf/onedriver/commit/4377d7562089b7725957e371671e86130322ff54
-        postInstall =
-          builtins.replaceStrings [ "resources/onedriver.desktop" ] [ "resources/onedriver-launcher.desktop" ]
-            oldAttrs.postInstall;
-      }
-    );
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will be accessible through 'pkgs.unstable'
