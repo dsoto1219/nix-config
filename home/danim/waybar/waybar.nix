40905fe5de0949896ac5e02452ef6141c6eb3997
dsoto1219 <dsotomail19@gmail.com>
2026-02-14 13:28:20 -0500

    Add extra right spacing to notifications

--- home/danim/waybar/waybar.nix
+++ home/danim/waybar/waybar.nix
@@ -101,7 +101,7 @@
 
     "custom/notifications" = {
       tooltip = true;
-      format = " {icon} ";
+      format = " {icon}  ";
       format-icons = {
         notification = "󱅫";
         none = "󰂜";
