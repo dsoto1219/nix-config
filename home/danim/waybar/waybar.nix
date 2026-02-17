91b9c8f7479fc0b96d25fd66c38f1ce60918b9e5
dsoto1219 <dsotomail19@gmail.com>
2026-02-16 21:06:39 -0500

    Move tray to modules-left

--- home/danim/waybar/waybar.nix
+++ home/danim/waybar/waybar.nix
@@ -20,6 +20,7 @@
 
     modules-left = [ 
       "hyprland/workspaces" 
+      "tray"
     ];
     modules-center = [
       "group/group-power" 
@@ -27,7 +28,6 @@
       "custom/file-manager" 
     ];
     modules-right = [
-      "tray"
       "pulseaudio"
       "backlight"
       "battery"
