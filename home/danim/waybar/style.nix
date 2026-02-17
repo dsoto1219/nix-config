43a8af234a24b1c0e9fdf4b0a75489361c0ddf04
dsoto1219 <dsotomail19@gmail.com>
2026-02-16 21:49:35 -0500

    Add slight opacity override

--- home/danim/waybar/style.nix
+++ home/danim/waybar/style.nix
@@ -3,7 +3,7 @@
   stylix.targets.waybar = {
     # enable = true;
     # addCss = true;
-    # opacity.override.desktop = 0.80; # override background opacity
+    opacity.override.desktop = 0.95; # override background opacity
   };
 
   programs.waybar.style = with config.lib.stylix.colors; (lib.mkAfter ''
