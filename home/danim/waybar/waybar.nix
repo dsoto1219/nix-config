a3f3d5365c01c6049a5967574bb2ef4469281250
dsoto1219 <dsotomail19@gmail.com>
2026-02-17 13:53:15 -0500

    waybar: remove space after volume-mute icon

--- home/danim/waybar/waybar.nix
+++ home/danim/waybar/waybar.nix
@@ -132,7 +132,7 @@
     pulseaudio = {
       format = "{icon} {volume}%";
       format-bluetooth = "{icon} {volume}%";
-      format-muted = "󰝟 ";
+      format-muted = "󰝟";
       format-icons = {
           headphone = " ";
           # hands-free = "";
