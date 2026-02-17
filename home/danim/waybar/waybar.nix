cdeb56afa0c4975eed4649a70c1ccd0c037583a8
dsoto1219 <dsotomail19@gmail.com>
2026-02-16 21:14:42 -0500

    Add right-space to window-rewrite-default icon

--- home/danim/waybar/waybar.nix
+++ home/danim/waybar/waybar.nix
@@ -37,7 +37,7 @@
     "hyprland/workspaces" = {
       format = "{windows}({icon})";
       # format-window-separator = "\n";
-      window-rewrite-default = "󰣆";
+      window-rewrite-default = "󰣆 ";
       window-rewrite = {
         "title<.*youtube.*>" = " "; # Windows whose titles contain "youtube"
         "class<firefox>" = " "; # Windows whose classes are "firefox"
