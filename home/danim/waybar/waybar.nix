d3f15a8d40d64924e2f08d529688c0b416f72ae2
dsoto1219 <dsotomail19@gmail.com>
2026-02-19 13:54:34 -0500

    waybar: remove space after power

--- home/danim/waybar/waybar.nix
+++ home/danim/waybar/waybar.nix
@@ -105,7 +105,7 @@
         tooltip-format = "Reboot";
     };
     "custom/power" = {
-        format = " ";
+        format = "";
         on-click = "hyprshutdown --post-cmd 'poweroff'";
         tooltip-format = "Power Off";
     };
