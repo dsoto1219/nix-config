5272212492fb941a43b51261b6cb68f6af7ccf15
dsoto1219 <dsotomail19@gmail.com>
2026-02-27 15:44:21 -0500

    Fix css indentation

--- home/danim/waybar/style.nix
+++ home/danim/waybar/style.nix
@@ -8,37 +8,37 @@
 
   programs.waybar.style = with config.lib.stylix.colors; (lib.mkAfter ''
     #workspaces {
-      /* padding: 5px 3px; */
-      margin: 8px;
-      border-radius: 12px;
-      border: 1px solid #${base01};
-      /* background: #${base01}; */
+        /* padding: 5px 3px; */
+        margin: 8px;
+        border-radius: 12px;
+        border: 1px solid #${base01};
+        /* background: #${base01}; */
     }
 
     #workspaces button:hover {
-      min-width: 50px;
-      transition: all 0.3s ease-in-out;
+        min-width: 50px;
+        transition: all 0.3s ease-in-out;
     }
 
     #workspaces button:not(:hover) {
-      transition: all 0.3s ease-in-out;
+        transition: all 0.3s ease-in-out;
     }
 
     #tray {
-      padding: 5px 12px;
-      margin: 15px 6px;
-      border-radius: 36px;
-      border-width: 1px;
-      background: #${base0D};
-      min-width: 32px;
+        padding: 5px 12px;
+        margin: 15px 6px;
+        border-radius: 36px;
+        border-width: 1px;
+        background: #${base0D};
+        min-width: 32px;
     }
 
     .modules-right {
-      padding: 5px 6px;
-      margin: 8px;
-      border-radius: 36px;
-      border-width: 1px;
-      background: #${base01};
+        padding: 5px 6px;
+        margin: 8px;
+        border-radius: 36px;
+        border-width: 1px;
+        background: #${base01};
     }
 
     #battery,
@@ -47,19 +47,15 @@
     #network,
     #clock,
     #custom-notifications {
-      margin: 0 3px;
-      padding: 10px 15px;
-      border-radius: 50px;
-      border-width: 1px;
+        margin: 0 3px;
+        padding: 10px 15px;
+        border-radius: 50px;
+        border-width: 1px;
     }
 
     #pulseaudio {
-      min-width: 30px;
-      color: #${base0A};
-    }
-
-    #clock {
-      color: #${base0E};
+        min-width: 30px;
+        color: #${base0A};
     }
 
     #battery.good {
@@ -74,42 +70,53 @@
       color: #${base0F};
     }
 
+    #clock {
+        color: #${base0E};
+    }
+
     #battery:hover,
     #backlight:hover,
     #pulseaudio:hover,
     #network:hover,
     #clock:hover,
     #custom-notifications:hover {
-      color: #${base01};
-      background-color: #${base05};
-      transition: all 0.3s ease-in-out;
+        color: #${base01};
+        background-color: #${base05};
+        transition: all 0.3s ease-in-out;
     }
 
     #pulseaudio:hover {
-      background-color: #${base0A};
+        background-color: #${base0A};
     }
 
+<<<<<<< HEAD
+=======
+    #backlight:hover {
+        background-color: #${base0C};
+    }
+
+>>>>>>> 0f6542d (Fix css indentation)
     #clock:hover {
-      background-color: #${base0E};
+        background-color: #${base0E};
     }
 
     #battery:hover {
-      background-color: #${base0F};
+        background-color: #${base0F};
     }
 
     .modules-center {
-      padding: 5px 10px;
-      margin: 8px;
-      border-radius: 36px;
-      border-width: 1px;
-      background: #${base01};
+        padding: 5px 10px;
+        margin: 8px;
+        border-radius: 36px;
+        border-width: 1px;
+        background: #${base01};
     }
 
     #custom-power,
     #custom-search,
     #custom-file-manager {
-      padding: 0 6px;
-      margin: 0 3px;
+        padding: 0 6px;
+        margin: 0 3px;
     }
   '');
 }
