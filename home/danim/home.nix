5dab1574e9e13a11487616146cb2832c3e9a3b4c
dsoto1219 <dsotomail19@gmail.com>
2026-03-03 12:53:39 -0500

    stylix: change polarity to dark

--- home/danim/home.nix
+++ home/danim/home.nix
@@ -33,6 +33,7 @@ in {
     enable = true;
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     image = ../../assets/sekiro-vs-father-sunset.png;
+    polarity = "dark";
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
