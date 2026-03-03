a842d72a88c73f812b80a186c1534a37fc5dea75
dsoto1219 <dsotomail19@gmail.com>
2026-03-03 14:17:44 -0500

    Stop monado systemd process when all OpenXR apps close

--- hosts/thinkbook/vr.nix
+++ hosts/thinkbook/vr.nix
@@ -4,4 +4,7 @@
     enable = true;
     defaultRuntime = true;
   };
+
+  # Stop monado systemd process when all XR apps close
+  systemd.user.services.monado.environment."IPC_EXIT_ON_DISCONNECT" = "1";
 }
