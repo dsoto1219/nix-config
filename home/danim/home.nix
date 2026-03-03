c0c39eda7e53b6d8f10cb7c4c2539f12cb992a84
dsoto1219 <dsotomail19@gmail.com>
2026-03-03 13:18:18 -0500

    Try monokai theme

--- home/danim/home.nix
+++ home/danim/home.nix
@@ -31,7 +31,7 @@ in {
 
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
     image = ../../assets/sekiro-vs-father-sunset.png;
     cursor = {
       package = pkgs.bibata-cursors;
