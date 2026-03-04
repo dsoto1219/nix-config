938a211b0c273103dd7ac803b4b1aebd4fc2225e
dsoto1219 <dsotomail19@gmail.com>
2026-03-04 16:01:10 -0500

    Disable rofi as stylix target

--- home/danim/rofi/default.nix
+++ home/danim/rofi/default.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }:
 {
+  stylix.targets.rofi.enable = false;
+
   programs.rofi = {
     enable = true; # application launcher
     # theme = builtins.readFile "${pkgs.rofi}/share/rofi/themes/Monokai.rasi";
