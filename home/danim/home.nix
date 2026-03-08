dfdb9ac58a0793ecaaee9c7ed89171a03fcff502
dsoto1219 <dsotomail19@gmail.com>
2026-03-08 12:46:59 -0400

    Replace steam home package with programs.steam attr set

--- home/danim/home.nix
+++ home/danim/home.nix
@@ -25,10 +25,15 @@ in {
     vesktop
     pamixer
     qimgv
-    steam
   ];
 
   programs.onedrive.enable = true;
+
+  programs.steam = {
+     enable = true;
+     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+   };
 
   stylix = {
     enable = true;
