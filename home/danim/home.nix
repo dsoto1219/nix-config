3f0b868ab0e8cb98ef3b7ffe21976d86ce219f42
dsoto1219 <dsotomail19@gmail.com>
2026-03-08 17:31:16 -0400

    Delete home.permanenece and relace with environment permanence in nixos config

--- home/danim/home.nix
+++ home/danim/home.nix
@@ -41,21 +41,4 @@ in {
       size = 16;
     };
   };
-
-  home.persistence."/persistent" = {
-    directories = [
-      "Documents"
-      "Downloads"
-      "Music"
-      "OneDrive"
-      "Pictures"
-      "Videos"
-      { directory = ".gnupg"; mode = "0700"; }
-      { directory = ".ssh"; mode = "0700"; }
-      { directory = ".nixops"; mode = "0700"; }
-      { directory = ".local/share/keyrings"; mode = "0700"; }
-      ".local/share/direnv"
-    ];
-    files = [ ".screenrc" ]; 
-  };
 }
