56116014ccd275b40b72f2b5ee9509e3afaa4df9
dsoto1219 <dsotomail19@gmail.com>
2026-03-08 13:36:57 -0400

    Change nixos home attr structure

--- home/nixos-wsl/home.nix
+++ home/nixos-wsl/home.nix
@@ -7,10 +7,8 @@
 }: let
   username = "nixos";
 in {
-  home = {
-    inherit username;
-    homeDirectory = "/home/${username}";
-  };
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
 
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
 }
