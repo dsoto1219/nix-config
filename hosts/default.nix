4b90479edabedf7def266364cd4d96a922375fc6
dsoto1219 <dsotomail19@gmail.com>
2026-03-10 13:08:01 -0400

    Add killall to system packages

--- hosts/default.nix
+++ hosts/default.nix
@@ -93,6 +93,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    killall
     tmux
     tree
     zip unzip
