9c7fafc87698a43d9941e69261d996e4e7fc5c90
dsoto1219 <dsotomail19@gmail.com>
2026-03-10 12:26:49 -0400

    set programs.onedrive.package to onedrivegui

--- home/danim/home.nix
+++ home/danim/home.nix
@@ -18,7 +18,6 @@ in {
 
   # Add stuff for your user as you see fit:
   home.packages = with pkgs; [ 
-    onedrivegui
     obsidian 
     kdePackages.ksshaskpass # for obsidian-git auth
     zotero
@@ -29,7 +28,10 @@ in {
     steamcmd steam-tui
   ];
 
-  programs.onedrive.enable = true;
+  programs.onedrive = {
+    enable = true;
+    package = pkgs.onedrivegui;
+  };
 
   stylix = {
     enable = true;
