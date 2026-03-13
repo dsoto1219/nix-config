cc7683700d0aa871ab179c9ef7e38cd35a4d70c8
dsoto1219 <dsotomail1219@gmail.com>
2026-03-12 20:13:56 -0400

    Comment out hyprpaper in exec-once

--- home/danim/hypr/hyprland.nix
+++ home/danim/hypr/hyprland.nix
@@ -67,7 +67,7 @@
         "systemctl --user start hyprpolkitagent"
         "hyprlock || hyprctl dispatch exit"
         # "$terminal"
-        "hyprpaper"
+        # "hyprpaper"
         "waybar"
         "swaync"
         "nm-applet &"
