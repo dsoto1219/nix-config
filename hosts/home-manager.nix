4783454db008a0c94e12f896446b7dae906dc95f
dsoto1219 <dsotomail1219@gmail.com>
2026-03-14 13:29:00 -0400

    Remove trailing space

--- hosts/home-manager.nix
+++ hosts/home-manager.nix
@@ -6,7 +6,7 @@
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  
+
   home-manager.extraSpecialArgs = { inherit inputs; };
 
   # Install home-manager tool globally
