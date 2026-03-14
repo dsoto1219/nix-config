2595ddaf1b335816ed21e4126b64ef4fa48c0517
dsoto1219 <dsotomail1219@gmail.com>
2026-03-14 13:35:01 -0400

    Revert "home-manager: Remove useGlobalPkgs option"
    This reverts commit 76aa9895ef807ee52782c34f58cabb7fc7a209dd.

--- hosts/home-manager.nix
+++ hosts/home-manager.nix
@@ -4,7 +4,7 @@
     inputs.home-manager.nixosModules.home-manager
   ];
 
-  # home-manager.useGlobalPkgs = true; # Not set: conflicts with nixpkgs options set in default.nix
+  home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
 
   home-manager.extraSpecialArgs = { inherit inputs; };
