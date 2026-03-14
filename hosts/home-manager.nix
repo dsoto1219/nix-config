d996115a8b90e1cac002f47d090eb6b1f3697ae3
dsoto1219 <dsotomail1219@gmail.com>
2026-03-14 13:22:24 -0400

    home-manager: Add useGlobalPkgs and useUserPackages

--- hosts/home-manager.nix
+++ hosts/home-manager.nix
@@ -1,4 +1,8 @@
 { inputs, pkgs, ... }: {
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  
   home-manager.extraSpecialArgs = { inherit inputs; };
 
   # Install home-manager tool globally
