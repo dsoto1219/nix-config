42220118a9ebc79a7f7dbb4e1acac43591bdd935
dsoto1219 <dsotomail1219@gmail.com>
2026-03-15 10:46:04 -0400

    Remove hyprland flake input following nixpkgs

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
 
     # Hyprland input gives us more control over plugins
     hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-25.11";
