612985d295c10751d561d9447d5b0c31f31143c9
dsoto1219 <dsotomail1219@gmail.com>
2026-03-15 13:14:20 -0400

    Use gruvbox stylix theme

--- home/danim/home.nix
+++ home/danim/home.nix
@@ -31,7 +31,7 @@ in {
 
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     image = ../../assets/sekiro-vs-father-sunset.png;
     cursor = {
       package = pkgs.bibata-cursors;
