5b59fbbf3bca94d7065cd90755556f8ac813e060
dsoto1219 <dsotomail1219@gmail.com>
2026-04-08 11:54:45 -0400

    Fix systemd rollback script

--- hosts/thinkbook/impermanence.nix
+++ hosts/thinkbook/impermanence.nix
@@ -1,41 +1,39 @@
 { lib, pkgs, ... }:
 {
-  # Overall systemd script from https://blog.decent.id/post/nixos-systemd-initrd###Impermanence%20rollback
-  boot.initrd.systemd.services.btrfs-cleanup = {
-    description = "Remove roots older than 30 days";
+  boot.initrd.systemd.services.rollback = {
+    description = "Rollback BTRFS root subvolume to blank snapshot";
     wantedBy = [ "initrd.target" ];
+    after = [ "systemd-hibernate-resume.service" ];
     before = [ "sysroot.mount" ];
-    requires = [ "dev-root_vg-root.device" ];  # <-- wait for LVM device node
-    path = with pkgs; [ 
-      btrfs-progs
-    ];
     unitConfig.DefaultDependencies = "no";
-    serviceConfig.Type = "oneshot";
-    # From: https://github.com/nix-community/impermanence?tab=readme-ov-file#btrfs-subvolumes
-    script = ''
-      mkdir /btrfs_tmp
-      mount /dev/root_vg/root /btrfs_tmp
-      if [[ -e /btrfs_tmp/root ]]; then
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = pkgs.writeShellScript "rollback" ''
+        mkdir /btrfs_tmp
+        mount /dev/root_vg/root /btrfs_tmp
+
+        if [[ -e /btrfs_tmp/root ]]; then
           mkdir -p /btrfs_tmp/old_roots
           timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
           mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-      fi
+        fi
 
-      delete_subvolume_recursively() {
+        delete_subvolume_recursively() {
           IFS=$'\n'
           for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
+            delete_subvolume_recursively "/btrfs_tmp/$i"
           done
           btrfs subvolume delete "$1"
-      }
+        }
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
           delete_subvolume_recursively "$i"
-      done
+        done
 
-      btrfs subvolume create /btrfs_tmp/root
-      umount /btrfs_tmp
-    '';
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+      '';
+    };
   };
 
   environment.persistence."/persistent" = {
