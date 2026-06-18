4ce0afed0d0802c37f51879698659f4be7cac0aa
dsoto1219 <dsotomail1219@gmail.com>
2026-06-18 10:03:39 -0400

    Add require oil setup

--- home/nvim/config/lua/custom/plugins/init.lua
+++ home/nvim/config/lua/custom/plugins/init.lua
@@ -13,6 +13,20 @@ vim.pack.add {
   gh 'Freedzone/kerbovim'
 }
 
+require("oil").setup({
+  -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+  -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+  default_file_explorer = true,
+  -- Id is automatically added at the beginning, and name at the end
+  -- See :help oil-columns
+  columns = {
+    "icon",
+    "permissions",
+    "size",
+    "mtime",
+  },
+})
+
 -- Iterate over all Lua files in the plugins directory and load them
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
 for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
