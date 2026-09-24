8f32b2ac75e5b5f4a82ae704625232c685895b38
dsoto1219 <dsotomail1219@gmail.com>
2026-09-24 14:01:15 -0400

    Add opts to rofi keybind command

--- home/danim/hypr/keybinds.nix
+++ home/danim/hypr/keybinds.nix
@@ -24,6 +24,7 @@ in {
     terminal    = { _var = "kitty"; };
     fileManager = { _var = "dolphin"; };
     menu        = { _var = "rofi"; };
+    menuOpts    = { _var = "-show drun"; };
 
     ###################
     ### KEYBINDINGS ###
@@ -39,7 +40,7 @@ in {
       (bind (mod "C") "hl.dsp.window.close()")
       (bind (mod "E") (execVar "fileManager"))
       (bind (mod "V") ''hl.dsp.window.float({ action = "toggle"})'')
-      (bind (mod "R") (execVar "menu"))
+      (bind (mod "R") (execVar "menu" + "menuOpts"))
       (bind (mod "P") ("hl.dsp.window.pseudo()"))
       (bind (mod "SHIFT + J") ''hl.dsp.layout("togglesplit")'') # dwindle only
 
